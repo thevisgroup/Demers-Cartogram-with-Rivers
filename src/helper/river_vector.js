6076d4544a9cb55363bb7e85efa74525b0c07444
Henry <hi@henry.wang>
2022-05-06 16:39:47 +0100

    fix: bounding box test

--- src/helper/river_vector.js
+++ src/helper/river_vector.js
@@ -1,10 +1,49 @@
-const bounding_box = (topLeft, bottomRight, point) => {
-  return (
-    topLeft.x <= point.x &&
-    point.x <= bottomRight.x &&
-    topLeft.y <= point.y &&
-    point.y <= bottomRight.y
-  );
+const bounding_box = (ref1, ref2, point) => {
+  ref1 = { x: ref1[0], y: ref1[1] };
+  ref2 = { x: ref2[0], y: ref2[1] };
+  // ref1 : top right
+  // ref2 : bottom left
+  if (ref1.x >= ref2.x && ref1.y < ref2.y) {
+    return (
+      point.x <= ref1.x &&
+      point.x >= ref2.x &&
+      point.y >= ref1.y &&
+      point.y <= ref2.y
+    );
+  }
+
+  // ref1 : bottom right
+  // ref2 : top left
+  else if (ref1.x >= ref2.x && ref1.y >= ref2.y) {
+    return (
+      point.x < ref1.x &&
+      point.x >= ref2.x &&
+      point.y < ref1.y &&
+      point.y >= ref2.y
+    );
+  }
+
+  // ref1 : top left
+  // ref2 : bottom right
+  else if (ref1.x < ref2.x && ref1.y < ref2.y) {
+    return (
+      point.x >= ref1.x &&
+      point.x < ref2.x &&
+      point.y >= ref1.y &&
+      point.y < ref2.y
+    );
+  }
+
+  // ref1 : bottom left
+  // ref2 : top right
+  else if (ref1.x < ref2.x && ref1.y >= ref2.y) {
+    return (
+      point.x >= ref1.x &&
+      point.x < ref2.x &&
+      point.y < ref1.y &&
+      point.y >= ref2.y
+    );
+  }
 };
 
 const cal_slope = (start, end) => {
